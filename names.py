aaa01b341d8bb09a95eb9174277900891487d05d
Kayne35Lewis <kn19lewi@siena.edu>
2025-09-29 12:38:13 -0400

    updated names.py to print our names in captial letters

--- names.py
+++ names.py
@@ -1,14 +1,15 @@
 # names.py
 # CSIS-110 Fall 2025
-# Ira Goldstein
+# John Kayne
 
 # Simple loop using range()
 
 # A function to print a series of names.
 # It does not have any parameters
 def printNames():
-  for myName in ["Chris", "Jamie", "Pat", "Taylor" ]:
-        print ( myName )
+  for myName in ["John", "Kayne"]:
+      upperName = myName.upper()
+      print (upperName)
 
 
 # Call our function
