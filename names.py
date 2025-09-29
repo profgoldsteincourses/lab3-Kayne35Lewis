baede88e2acaeae538c39a98e5e3a6cd04faec88
JohnM-Siena <jd26manu@siena.edu>
2025-09-29 12:01:38 -0400

    updated names,py to print our names

--- names.py
+++ names.py
@@ -1,13 +1,13 @@
 # names.py
 # CSIS-110 Fall 2025
-# Ira Goldstein
+# JohnManuyag, Kayne Lewis
 
 # Simple loop using range()
 
 # A function to print a series of names.
 # It does not have any parameters
 def printNames():
-  for myName in ["Chris", "Jamie", "Pat", "Taylor" ]:
+  for myName in ["John", "Kayne"]:
         print ( myName )
 
 
