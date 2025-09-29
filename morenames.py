734c9c349c5b20ccbdb38940c3221139e0321b05
JohnM-Siena <jd26manu@siena.edu>
2025-09-29 13:22:15 -0400

    added morenames.py to demomstrate nested loops

---
+++ morenames.py
@@ -0,0 +1,15 @@
+# names.py
+# CSIS-110 Fall 2025
+# John Kayne
+# JohnManuyag, Kayne Lewis
+
+# Simple loop using range()
+
+# A function to print a series of names.
+# It does not have any parameters
+def printNames():
+  for myName in ["Drake", "Taylot", "Chappell"]:
+      for lastName in ["Graham", "Swift", "Roan"]:
+        print ( myName + " " + lastName )
+# Call our function
+printNames()
