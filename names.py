e6987691b50978fa1bc82977b2d7cbbad84cd776
JohnM-Siena <jd26manu@siena.edu>
2025-10-05 13:55:21 -0400

    fixed garbage in names.py
    removed random garbage code in names.py that caused code to malfunction

--- names.py
+++ names.py
@@ -1,24 +1,14 @@
 # names.py
 # CSIS-110 Fall 2025
-<<<<<<< HEAD
 # John Kayne
-=======
 # JohnManuyag, Kayne Lewis
->>>>>>> baede88e2acaeae538c39a98e5e3a6cd04faec88
-
 # Simple loop using range()
-
 # A function to print a series of names.
 # It does not have any parameters
 def printNames():
   for myName in ["John", "Kayne"]:
-<<<<<<< HEAD
       upperName = myName.upper()
       print (upperName)
-=======
-        print ( myName )
->>>>>>> baede88e2acaeae538c39a98e5e3a6cd04faec88
-
-
+      print ( myName )
 # Call our function
 printNames()
